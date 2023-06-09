92749dd68745c9b02a805eff609c7bf287cc9d36
MMG956 <67861460+MMG956@users.noreply.github.com>
2023-06-09 19:06:52 +0800

    Create P14数组的中心位置.java

---
+++ src/main/java/OD真题/P14数组的中心位置.java
@@ -0,0 +1,32 @@
+package OD真题;
+
+import java.util.Scanner;
+
+/**
+ * 给你一个整数数组nums，请计算数组的中心位置，数组中心位置是数组的一个下标，其左侧所有元素的乘积等于右侧所有元素的乘积。
+ * 数组第一个元素的左侧积为1，最后一个元素的右侧积也为1；
+ * 如果数组中有多个中心位置，应返回最靠近左边的那一个。如果不存在中心位置，返回-1
+ *
+ * 输入描述
+ * 输入只有一行，给出N个正整数用空格分隔：nums = 2 5 3 6 5 6
+ * 1 <= nums.length <=1024
+ * 1 <= nums[i] <= 10
+ *
+ * 输出描述：    3
+ *
+ * 示例1：
+ *  输入：
+ *      2 5 3 6 5 6
+ *  输出：
+ *      3
+ *
+ * @since 2023-06-09
+ */
+public class P14数组的中心位置 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String[] line = s.nextLine().split(" ");
+
+
+    }
+}
